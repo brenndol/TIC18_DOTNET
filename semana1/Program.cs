aa0118f8799d06bf8e95ebe3a99fe6c99df0599a
brenndol <brendo1994@gmail.com>
2023-11-15 17:19:44 -0300

    tipos de dados e conversão

--- semana1/Program.cs
+++ semana1/Program.cs
@@ -1,2 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+#region
+//tipo de dados
+int tipoInteiro;
+double tipoDouble = 10.75;
+bool tipoBool;
+string tipoString;
+long tipoLong;
+
+//convertendo double em int
+static void ()
+{
+int tipoInteiro = (int) tipoBool;
+Console.WriteLine($"Double: {tipoDouble}" );
+Console.WriteLine($"Inteiro: {tipoInteiro}");
+}
+
+#endregion
+
+
+
+
+
+
+
+
+
+
+
